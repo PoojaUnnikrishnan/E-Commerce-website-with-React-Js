a53d208dcac8c4c267bb77c342bf8bcada9bfb39
pooja unnikrishnan <poojaunnikrishnan7558@gmail.com>
2022-05-08 09:40:48 +0530

    E_commerce

---
+++ src/components/CheckoutProduct.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./CheckoutProduct.css";
+// accessing contents in data layer
+import { useStateValue } from "./StateProvider";
+
+function CheckoutProduct({ id, image, title, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+  const removeFromBasket = () => {
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  };
+  return (
+    <div className="checkoutproduct">
+      <img className="checkoutProduct_image" src={image} alt={title} />
+      <div checkoutProduct_info>
+        <p className="checkoutProduct_title">{title}</p>
+        <p className="checkoutProduct_price">
+          <small>₹</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="checkoutProduct_rating">
+          {/* fill will fill the array with empty elements, and according to index 'i' starts will be added  */}
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
+        </div>
+        <button onClick={removeFromBasket}>Remove From Basket</button>
+      </div>
+    </div>
+  );
+}
+
+export default CheckoutProduct;
