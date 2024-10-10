5483bcb79fb31290d514afe5bd180c11d5f164aa
Pooja Unnikrishnan <100412366+PoojaUnnikrishnan@users.noreply.github.com>
2024-10-10 15:08:05 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBbuIuY0se-Af1zpgS-zcMzZ6nD3Rc_lRA",
-  authDomain: "fir-e3a5d.firebaseapp.com",
-  projectId: "fir-e3a5d",
-  storageBucket: "fir-e3a5d.appspot.com",
-  messagingSenderId: "846267714089",
-  appId: "1:846267714089:web:4cbc233b500db33da2aa38",
-  measurementId: "G-DWKQKWRZ2Y",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_MEASUREMENT_ID,
 };
 //initialize firebase app
 const firebaseApp = firebase.initializeApp(firebaseConfig);
